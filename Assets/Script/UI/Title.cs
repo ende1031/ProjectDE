6a76d6b49bc0228e2f88ee7a5c4c8ad5aa9171be
28-A402\Administrator <ende1031@naver.com>
2018-02-22 12:38:54 +0900

    타이틀화면 작동되게, 인벤토리뚜껑 흐려보이는 문제 해결

--- Assets/Script/UI/Title.cs
+++ Assets/Script/UI/Title.cs
@@ -4,64 +4,147 @@ using UnityEngine;
 
 public class Title : MonoBehaviour
 {
-    GameObject SelectCursor;
+    GameObject Cursor;
+    GameObject[] titleMenu;
+    Animator animaitor;
+    GameObject BlackScreen;
 
     int selectIndex = 0;
 
     bool isCursorActive = false;
     float Timer = 0;
+    bool isSelect = false;
 
     void Start ()
     {
-        SelectCursor = GameObject.Find("Title_Select");
+        Cursor = GameObject.Find("Cursor");
+        titleMenu = new GameObject[5] { GameObject.Find("NewStart"), GameObject.Find("Load"), GameObject.Find("Quit"), GameObject.Find("Setting"), GameObject.Find("People") };
+        BlackScreen = GameObject.Find("Canvas").transform.Find("BlackScreen").gameObject;
+        animaitor = GetComponent<Animator>();
+        RefreshMenu();
     }
 	
 	void Update ()
     {
-        Timer += Time.deltaTime;
-
-        if (isCursorActive == true)
+        if (isSelect == false)
         {
-            if (Input.GetKeyDown(KeyCode.UpArrow) && selectIndex > 0)
+            if (isCursorActive == true)
             {
-                selectIndex--;
-            }
-            if (Input.GetKeyDown(KeyCode.DownArrow) && selectIndex < 1)
-            {
-                selectIndex++;
-            }
-
-            Vector3 tempPos = SelectCursor.transform.position;
-
-            switch (selectIndex)
-            {
-                case 0:
-                    tempPos.x = 981.0f;
-                    tempPos.y = 290.0f;
-
-                    if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.C))
+                if (Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    if (selectIndex == 0)
                     {
-                        GameStart();
+                        selectIndex = 4;
                     }
-                    break;
-                case 1:
-                    tempPos.x = 1114.0f;
-                    tempPos.y = 190.0f;
-
-                    if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.C))
+                    else
                     {
-                        Application.Quit();
+                        selectIndex--;
                     }
-                    break;
-            }
+                    RefreshMenu();
+                }
+                else if (Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    if (selectIndex == 4)
+                    {
+                        selectIndex = 0;
+                    }
+                    else
+                    {
+                        selectIndex++;
+                    }
+                    RefreshMenu();
+                }
+                else if (Input.GetKeyDown(KeyCode.RightArrow) && selectIndex <= 2)
+                {
+                    selectIndex = 3;
+                    RefreshMenu();
+                }
+                else if (Input.GetKeyDown(KeyCode.LeftArrow) && selectIndex > 2)
+                {
+                    selectIndex = 0;
+                    RefreshMenu();
+                }
 
-            SelectCursor.transform.position = tempPos;
+                if (Input.GetKeyDown(KeyCode.C) || Input.GetKeyDown(KeyCode.Return))
+                {
+                    SelectMenu();
+                }
+            }
+            else
+            {
+                Timer += Time.deltaTime;
+                if (Timer > 0.5f)
+                {
+                    isCursorActive = true;
+                }
+            }
         }
         else
         {
-            if(Timer > 0.5f)
+            Color tempColor;
+            for (int i = 0; i < 5; i++)
             {
-                isCursorActive = true;
+                tempColor = titleMenu[i].GetComponent<TextMesh>().color;
+                if (tempColor.a > 0.1f)
+                {
+                    tempColor.a -= Time.deltaTime * 3;
+                }
+                else
+                {
+                    tempColor.a = 0;
+                }
+                titleMenu[i].GetComponent<TextMesh>().color = tempColor;
+            }
+
+            tempColor = Cursor.GetComponent<SpriteRenderer>().color;
+            if (tempColor.a > 0.1f)
+            {
+                tempColor.a -= Time.deltaTime * 5;
+            }
+            else
+            {
+                tempColor.a = 0;
+            }
+            Cursor.GetComponent<SpriteRenderer>().color = tempColor;
+        }
+    }
+
+    void SelectMenu()
+    {
+        switch(selectIndex)
+        {
+            case 0:
+                animaitor.SetBool("isStart", true);
+                BlackScreen.SetActive(true);
+                isSelect = true;
+                break;
+
+            case 2:
+                isSelect = true;
+                Application.Quit();
+                break;
+        }
+    }
+
+    void RefreshMenu()
+    {
+        Vector3 tempPos = titleMenu[selectIndex].transform.position;
+        tempPos.z = -1.2f;
+        Cursor.transform.position = tempPos;
+        Cursor.transform.rotation = titleMenu[selectIndex].transform.rotation;
+
+        Color tempColor = titleMenu[0].GetComponent<TextMesh>().color;
+        for(int i =0; i<5; i++)
+        {
+            if(i == selectIndex)
+            {
+                tempColor.a = 1;
+                titleMenu[i].GetComponent<TextMesh>().color = tempColor;
+            }
+            else
+            {
+                tempColor.a = 0.5f;
+                titleMenu[i].GetComponent<TextMesh>().color = tempColor;
             }
         }
     }
